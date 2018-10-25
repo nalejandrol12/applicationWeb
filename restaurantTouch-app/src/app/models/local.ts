bd7107e7f86d3df4e8671e69468d16170ea926f7
alejandro <alejandro121940@gmail.com>
2018-10-24 23:09:55 -0500

    form

---
+++ restaurantTouch-app/src/app/models/local.ts
@@ -0,0 +1,14 @@
+export class LocalForm{
+    public name:string;
+    public email:string;
+    public password:string;
+    public phone:number;
+    public address:string;
+    public horaInicial:string;
+    public horarioInicial:string;
+    public horaFinal:string;
+    public horarioFinal:string;
+    public description:string;
+    public image:FileList;
+    
+}
--- restaurantTouch-app/src/app/sign-up/sign-up.component.ts
+++ restaurantTouch-app/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, Validators } from '@angular/forms';
+
+import { LocalForm } from '../models/local';
+
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  LocalModel = new LocalForm();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(f: NgForm) {
+    this.getSentServices(this.LocalModel, f);
+  }
+
+  getSentServices(body: LocalForm, f: NgForm) {
+
+    console.log(body)
+  }
 }
